252812b9ac902a7f0737aed3f54bc5a9e02bcee2
Bil <clebiojuniorrr@gmail.com>
2024-08-14 21:16:15 -0300

    .

---
+++ app/back_end/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, '..', 'front_end')));
+
+app.get('/', (req, res) => {
+  try {
+    res.sendFile(path.join(__dirname, '..', 'front_end', 'index.html'));
+  } catch (err) {
+    res.status(400).send('Bad Request');
+  }
+});
+
+app.listen(port, () => {
+  console.log("server running sucessfuly!")
+});

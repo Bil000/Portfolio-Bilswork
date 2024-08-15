02a40826fa04ec8f72ac7e1a31ca1ccc6c2fd2b0
Bil <clebiojuniorrr@gmail.com>
2024-08-15 05:35:44 -0300

    .

--- public/app/back_end/api/index.js
+++ public/app/back_end/api/index.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000;
 
-const port = 3000;
-
-app.use(express.static(path.join(__dirname, '..', 'front_end')));
+app.use(express.static(path.join(__dirname, '..', '..', 'public', 'app', 'front_end')));
 
 app.get('/', (req, res) => {
-  try {
-    res.sendFile(path.join(__dirname, '..', 'front_end', 'index.html'));
-  } catch (err) {
-    res.status(400).send('Bad Request');
-  }
+  res.sendFile(path.join(__dirname, '..', '..', 'public', 'app', 'front_end', 'index.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).send('Not Found');
 });
 
 app.listen(port, () => {
-  console.log("server running sucessfuly!")
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
 module.exports = app;

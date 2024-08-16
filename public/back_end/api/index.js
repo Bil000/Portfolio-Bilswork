abb1dc5b50b76ca88141a1d889f4f100b8022922
Bil <clebiojuniorrr@gmail.com>
2024-08-15 22:26:32 -0300

    .

---
+++ public/back_end/api/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+app.use(express.static(path.join(__dirname, '..', '..', 'public')));
+app.use('archive', express.static(path.join(__dirname, '..', '..', 'archive')))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '..', 'front_end', 'index.html'));
+});
+app.get('/styles/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '..', 'front_end', 'style.css'));
+});
+
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
+module.exports = app;

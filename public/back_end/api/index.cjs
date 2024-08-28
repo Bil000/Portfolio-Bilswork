d18ff0dee5dfc186188d7030b2e0396c05ca0c13
penscom <clebiodesouza22@gmail.com>
2024-08-27 21:36:50 -0300

    created a route path for links.mjs

---
+++ public/back_end/api/index.cjs
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, '..', '..', 'public')));
+app.use('/assets', express.static(path.join(__dirname,'..', '..', 'archive')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '..', 'front_end', 'index.html'));
+});
+app.get('/styles/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '..', 'front_end', 'style.css'));
+});
+app.get('/cloudinary', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'cloudinary.js'));
+});
+app.get('/routes', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '..', 'front_end', 'links.mjs'));
+});
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
+module.exports = app;
